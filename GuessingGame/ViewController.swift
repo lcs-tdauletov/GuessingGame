245130f82417b39c0011789b6af38b6b28632f03
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2018-01-16 13:58:13 -0500

    Committed to the new piece of information. connected view to controller.

--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var numberGuessed: UITextField!
+    
+    
+    
+    // MARK: Properties (variables)
+    
+    // MARK: Overrides
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +28,40 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // MARK: Actions
+    
+    @IBAction func guessAction(_ sender: Any) {
+        
+        // Print the number that was guessed
+        
+         print(numberGuessed.text)
+        
+        //Use a guard statement to bind to a non-optional variable
+        guard let inputGiven = numberGuessed.text else {
+            //If the input was nill, stop and return (exit) the function
+            
+            return
+        }
+        
+        //Use a guard statement to attempt to create an integer
+        guard let integerGiven = Int(inputGiven) else {
+            // If the input was text( e.g. : "five") we can't make an integer, so sto and return (exit) the function
+            
+            return
+        }
+        
+         // Print the number that was guessed
+        print(integerGiven)
+    }
+    
+   
+    
+    
+    
+    
+    // MARK: Custom Function(s)
 
 
 }
